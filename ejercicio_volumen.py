4b8e85dac82168adf9e4d55902377a5ed8484c26
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2024-06-11 17:49:38 -0300

    Calcular el volumen de una caja

--- ejercicio_volumen.py
+++ ejercicio_volumen.py
@@ -14,6 +14,5 @@ ancho = float(input("Coloca el ancho de la caja: "))
 alto = float(input("Coloca el alto de la caja: "))
 
 cajita = Caja(largo,ancho,alto)
-# se agregan dos formas de imprimir en pantalla
-# print("el volumen de la caja es: ", cajita.calcularVolumen())	
-print(f"el valor del volumen de la caja es: {(cajita.calcularVolumen())}")			
+# se agregan dos formas de imprimir en pantalla# print("el volumen de la caja es: ", cajita.calcularVolumen())	
+print(f"el valor del volumen de la caja es: {round((cajita.calcularVolumen()))}")			
